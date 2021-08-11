f70e7cc2c65d74066a04a6ce0cbd4c10a3f2b6f0
Dominik Fuchs <d0minik.fuchs@web.de>
2021-08-11 12:15:13 +0200

    fix two more tests

--- src/test/javascript/spec/component/shared/participant-scores-average-table.component.spec.ts
+++ src/test/javascript/spec/component/shared/participant-scores-average-table.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ParticipantScoresAverageTableComponent } from 'app/shared/participant-scores/participant-scores-average-table/participant-scores-average-table.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { TranslateModule } from '@ngx-translate/core';
-import { MockPipe } from 'ng-mocks';
+import { MockDirective, MockPipe } from 'ng-mocks';
 import * as sinonChai from 'sinon-chai';
 import * as sinon from 'sinon';
 import * as chai from 'chai';
